67dd2801cf121922dc8a4d816392b515c43defce
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2022-10-24 17:11:25 +0200

    compiled py were dirty on the robot

--- python/reachy_sdk_api/orbita_kinematics_pb2.py
+++ python/reachy_sdk_api/orbita_kinematics_pb2.py
@@ -3,6 +3,7 @@
 # source: orbita_kinematics.proto
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
@@ -14,54 +15,11 @@ _sym_db = _symbol_database.Default()
 import kinematics_pb2 as kinematics__pb2
 
 
-DESCRIPTOR = _descriptor.FileDescriptor(
-  name='orbita_kinematics.proto',
-  package='reachy.sdk.kinematics',
-  syntax='proto3',
-  serialized_options=None,
-  create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x17orbita_kinematics.proto\x12\x15reachy.sdk.kinematics\x1a\x10kinematics.proto\"?\n\x0fOrbitaIKRequest\x12,\n\x01q\x18\x01 \x01(\x0b\x32!.reachy.sdk.kinematics.Quaternionb\x06proto3'
-  ,
-  dependencies=[kinematics__pb2.DESCRIPTOR,])
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17orbita_kinematics.proto\x12\x15reachy.sdk.kinematics\x1a\x10kinematics.proto\"?\n\x0fOrbitaIKRequest\x12,\n\x01q\x18\x01 \x01(\x0b\x32!.reachy.sdk.kinematics.Quaternionb\x06proto3')
 
 
 
-
-_ORBITAIKREQUEST = _descriptor.Descriptor(
-  name='OrbitaIKRequest',
-  full_name='reachy.sdk.kinematics.OrbitaIKRequest',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='q', full_name='reachy.sdk.kinematics.OrbitaIKRequest.q', index=0,
-      number=1, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=68,
-  serialized_end=131,
-)
-
-_ORBITAIKREQUEST.fields_by_name['q'].message_type = kinematics__pb2._QUATERNION
-DESCRIPTOR.message_types_by_name['OrbitaIKRequest'] = _ORBITAIKREQUEST
-_sym_db.RegisterFileDescriptor(DESCRIPTOR)
-
+_ORBITAIKREQUEST = DESCRIPTOR.message_types_by_name['OrbitaIKRequest']
 OrbitaIKRequest = _reflection.GeneratedProtocolMessageType('OrbitaIKRequest', (_message.Message,), {
   'DESCRIPTOR' : _ORBITAIKREQUEST,
   '__module__' : 'orbita_kinematics_pb2'
@@ -69,5 +27,9 @@ OrbitaIKRequest = _reflection.GeneratedProtocolMessageType('OrbitaIKRequest', (_
   })
 _sym_db.RegisterMessage(OrbitaIKRequest)
 
+if _descriptor._USE_C_DESCRIPTORS == False:
 
+  DESCRIPTOR._options = None
+  _ORBITAIKREQUEST._serialized_start=68
+  _ORBITAIKREQUEST._serialized_end=131
 # @@protoc_insertion_point(module_scope)
