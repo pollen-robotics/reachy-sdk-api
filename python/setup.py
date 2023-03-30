ad9513bed161e04261ac1c8e14a22769b6550f3b
cdussieux <clementdussieux@hotmail.fr>
2023-03-30 10:59:01 +0200

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, '..', 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='reachy-sdk-api',
-    version='0.7.0',
+    version='0.7.1',
     packages=find_packages(exclude=['tests']),    
     install_requires=[
         'grpcio',
